b9d679b530c6d7d470c756e49c32906bcdab8e9d
Antoine <antoine.fernandez@rubrash.com>
2022-12-22 12:11:21 +0100

    feat: adding login screen, text input widget, instagram logo svg.

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:instagram/utils/colors.dart';
+import 'package:instagram/widget/text_field_input.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 32),
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Flexible(
+                flex: 2,
+                child: Container(),
+              ),
+              SvgPicture.asset(
+                "assets/ic_instagram.svg",
+                color: primaryColor,
+                height: 64,
+              ),
+              const SizedBox(
+                height: 64,
+              ),
+              TextFieldInput(
+                textEditingController: _emailController,
+                hintText: "Enter your email",
+                textInputType: TextInputType.emailAddress,
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              TextFieldInput(
+                textEditingController: _passwordController,
+                hintText: "Enter your password",
+                textInputType: TextInputType.text,
+                isPassword: true,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
