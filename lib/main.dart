1418ceb7b61a13b181be0a644b9ef55833c4250a
Antoine <antoine.fernandez@rubrash.com>
2022-12-22 11:14:23 +0100

    feat: setting up firebase.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,26 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram/responsive/mobile_screen_layout.dart';
 import 'package:instagram/responsive/responsive_layout_dart.dart';
 import 'package:instagram/responsive/web_screen_layout.dart';
 import 'package:instagram/utils/colors.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: "AIzaSyAp3ayAYoO3mO9lS7H-he_EzPZPLG9prFo",
+        appId: "1:819053432548:web:8edf3e834dc214674a26f1",
+        messagingSenderId: "819053432548",
+        projectId: "flutter-instagram-f27bd",
+        storageBucket: "flutter-instagram-f27bd.appspot.com",
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
